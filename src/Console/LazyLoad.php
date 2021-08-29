40d38569f8ad049437409fc22b8b5b2083f13f1c
llabbasmkhll <llabbasmkhll@gmail.com>
2021-08-30 01:31:47 +0430

    extract column names and types from migration files

--- src/Console/LazyLoad.php
+++ src/Console/LazyLoad.php
@@ -3,6 +3,7 @@
 namespace Llabbasmkhll\LaravelLazy\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 use Llabbasmkhll\LaravelLazy\Paths\FilePath;
 use Llabbasmkhll\LaravelLazy\Paths\LaravelPaths;
 
@@ -14,6 +15,45 @@ class LazyLoad extends Command
 
     public function handle()
     {
-//        explode("\n", file_get_contents(FilePath::getAbsFilePaths(LaravelPaths::migrationDirs())[0]));
+        foreach (FilePath::getAbsFilePaths(LaravelPaths::migrationDirs()) as $migration) {
+            $tables = $this->separateTables(file_get_contents($migration));
+            foreach ($tables as $table) {
+                $model   = $this->getTablesName($table);
+                $columns = $this->getColumns($table);
+
+                $this->info($model);
+                foreach ($columns as $column) {
+                    $this->info($column[0].' => '.$column[1]);
+                }
+            }
+        }
+    }
+
+    public function separateTables($file): array
+    {
+        $separated = explode('Schema::create', $file);
+        array_shift($separated);
+
+        return $separated;
+    }
+
+    public function getTablesName($table): string
+    {
+        return Str::singular(explode("'", $table)[1]);
+    }
+
+    public function getColumns($table): array
+    {
+        $columns = [];
+
+        foreach (explode("\n", $table) as $line) {
+            if (Str::containsAll($line, ['$table->', "'"])) {
+                $type = explode('(', explode('$table->', $line)[1])[0];
+                $name = explode("'", $line)[1];
+                array_push($columns, [$type, $name]);
+            }
+        }
+
+        return $columns;
     }
 }
